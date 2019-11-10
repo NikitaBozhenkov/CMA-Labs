3dde7b8c3f83fa6c5e9b4c1c332840ffcfb12431
Nikita Bozhenkov <nikbozhenkov@gmail.com>
2019-11-10 15:44:42 +0300

    Task 3 fixed

--- Operations-with-matrix/LU & LDLT/main.cpp
+++ Operations-with-matrix/LU & LDLT/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <ctime>
 
 #define Matrix std::vector<std::vector<double>>
 
@@ -74,37 +75,26 @@ _inline void LDLT_Equation(Matrix& matrix, std::vector<double>& B) {
       temp = std::sqrt(matrix[i][i]);
     }
 
-    // row div by sqrt
-    for(int j = i; j < order; ++j) {
-      matrix[i][j] /= temp;
-    }
-
     // Gauss
     for(int j = i + 1; j < order; ++j) {
-      double mul_value = matrix[j][i] / matrix[i][i];
-      for(int t = i; t < order; ++t) {
+      double mul_value = matrix[i][j] / matrix[i][i];
+      for(int t = j; t < order; ++t) {
         matrix[j][t] -= matrix[i][t] * mul_value;
       }
     }
-  }
 
-  Matrix L(order);
-
-
-
-  for(int i = 0; i < order; ++i) {
-    L[i].resize(order);
-    for(int j = 0; j < order; ++j) {
-      L[i][j] = matrix[j][i];
+    // row div by sqrt
+    for(int j = i; j < order; ++j) {
+      matrix[i][j] /= temp;
     }
   }
 
   //LY_1 = B
   for(int i = 0; i < order; ++i) {
     if (std::abs(B[i]) > 0.00001) {
-      B[i] /= L[i][i];
+      B[i] /= matrix[i][i];
       for(int j = i + 1; j < order; ++j) {
-        B[j] -= B[i] * L[j][i];
+        B[j] -= B[i] * matrix[i][j];
       }
     }
   }
@@ -137,18 +127,43 @@ int main() {
   }
 
   for(size_t i = 0; i < order; ++i) {
-    for(size_t j = 0; j < order; ++j) {
-      std::cin >> matrix[i][j];
-      //matrix[i][j] = (rand() % 100) * 0.1;
+    for(size_t j = i; j < order; ++j) {
+      //std::cin >> matrix[i][j];
+      matrix[i][j] = (rand() % 100) * 0.1;
+      if (i != j) {
+        matrix[j][i] = matrix[i][j];
+      }
     }
   }
 
-  std::vector<double> B = {16, -5, -30};
-  //LDLT_Equation(matrix, B);
-  LU_Equation(matrix, B);
+  std::vector<double> true_X(order);
+  for(int i = 0; i < order; ++i) {
+    true_X[i] = (rand() % 100) * 0.1;
+  }
 
-  for(const auto& elem : B) {
-    std::cout << elem << " ";
+  std::vector<double> B(order);
+  for(int i = 0; i < order; ++i) {
+    double sum = 0;
+    for(int j = 0; j < order; ++j) {
+      sum += matrix[i][j] * true_X[j];
+    }
+    B[i] = sum;
+  }
+
+  clock_t start_time = clock();
+
+  LDLT_Equation(matrix, B);
+  //LU_Equation(matrix, B);
+
+  clock_t end_time = clock();
+
+  long double search_time = (long double) (end_time - start_time) / CLOCKS_PER_SEC;
+  std::cout << "Time:" << search_time << std::endl;
+
+  for(int i = 0; i < order; ++i) {
+    if (std::abs(true_X[i] - B[i]) > 0.00001) {
+      std::cout << "BAD";
+    }
   }
   return 0;
 }
