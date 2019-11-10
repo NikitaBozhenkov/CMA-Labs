7d96e5c5cf50434e30d4adcdd4d8c6ed33f95079
Nikita Bozhenkov <nikbozhenkov@gmail.com>
2019-11-10 15:45:01 +0300

    Task 1 fixed

--- Operations-with-matrix/Inverse-matrix/main.cpp
+++ Operations-with-matrix/Inverse-matrix/main.cpp
@@ -58,7 +58,7 @@ int main() {
   for(size_t i = 0; i < order; ++i) {
     E[i][i] = 1;
     for(size_t j = 0; j < std::min(order, i + 2); ++j) {
-        //std::cin >> matrix[i][j];
+      //std::cin >> matrix[i][j];
       matrix[i][j] = (rand() % 100) * 0.1;
     }
     for(size_t j = order - 1; j >= i + 2; --j) {
@@ -71,14 +71,14 @@ int main() {
 
   for(int i = order - 1; i > 0; --i) {
     //finding max
-    if (std::abs(matrix[i][i]) < std::abs(matrix[i-1][i])) {
-      matrix[i].swap(matrix[i-1]);
-      E[i-1].swap(E[i]);
+    if (std::abs(matrix[i][i]) < std::abs(matrix[i - 1][i])) {
+      matrix[i].swap(matrix[i - 1]);
+      E[i - 1].swap(E[i]);
     }
 
     // Dividing by [i][i]-elem
     double div_value = matrix[i][i];
-    for(int j = i-1; j >= 0; --j) {
+    for(int j = i - 1; j >= 0; --j) {
       matrix[i][j] /= div_value;
     }
     for(auto& elem : E[i]) {
@@ -86,12 +86,12 @@ int main() {
     }
 
     //Subtracting rows
-    double mul_value = matrix[i-1][i];
-    for(int j = i-1; j >= 0; --j) {
-      matrix[i-1][j] -= matrix[i][j] * mul_value;
+    double mul_value = matrix[i - 1][i];
+    for(int j = i - 1; j >= 0; --j) {
+      matrix[i - 1][j] -= matrix[i][j] * mul_value;
     }
     for(int j = 0; j < order; ++j) {
-      E[i-1][j] -= E[i][j] * mul_value;
+      E[i - 1][j] -= E[i][j] * mul_value;
     }
   }
 
@@ -100,14 +100,14 @@ int main() {
   }
 
   for(int i = 0; i <= order - 1; ++i) {
-    if(std::abs(matrix[i][i]) >= 0.00001) {
+    if (std::abs(matrix[i][i]) >= 0.00001) {
     } else {
       std::cout << "TRUBA" << std::endl;
       return 0;
     }
     for(int j = i + 1; j < order; ++j) {
       for(int t = 0; t < order; ++t) {
-        E[j][t] -= E[i][t]*matrix[j][i];
+        E[j][t] -= E[i][t] * matrix[j][i];
       }
     }
   }
@@ -131,9 +131,9 @@ int main() {
 //
 //  for(int i = 0; i < order; ++i) {
 //    for(int j = 0; j < order; ++j) {
-//      std::cout << E[i][j] << " ";
+//      std::cout << E[i][j] << " & ";
 //    }
-//    std::cout << std::endl;
+//    std::cout << "\\\\" << std::endl;
 //  }
   return 0;
 }
